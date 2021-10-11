7d796350b2c55e20f9864efb205f9d79b54cf103
ANRanga <anranga95@gmail.com>
2021-10-11 01:11:32 -0400

    Webpack4, app meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,6 +15,16 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="Access to the decentralised world of Celo"
         />
 
+          <meta name='application-name' content='Plock.fi' />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+          <meta name='apple-mobile-web-app-title' content='Plock.fi' />
+          <meta name='description' content='Gateway to Defi on Celo' />
+          <meta name='format-detection' content='telephone=no' />
+          <meta name='mobile-web-app-capable' content='yes' />
+          <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
+          <link rel='manifest' href='/manifest.json' />
+
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="/icons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
         <link href="/icons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
