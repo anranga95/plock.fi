aa1437676c53055ea3ebb170228e9571f6c02b5a
ANRanga <anranga95@gmail.com>
2021-10-11 01:18:58 -0400

    Tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,20 +15,32 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="Access to the decentralised world of Celo"
         />
 
-          <meta name='application-name' content='Plock.fi' />
-          <meta name='apple-mobile-web-app-capable' content='yes' />
-          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
-          <meta name='apple-mobile-web-app-title' content='Plock.fi' />
-          <meta name='description' content='Gateway to Defi on Celo' />
-          <meta name='format-detection' content='telephone=no' />
-          <meta name='mobile-web-app-capable' content='yes' />
-          <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
-          <link rel='manifest' href='/manifest.json' />
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Description" />
+        <meta name="keywords" content="Keywords" />
+        <title>Next.js PWA Example</title>
 
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link href="/icons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
-        <link href="/icons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
-
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/icons/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/icons/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#317EFB" />
+          
         <script async src="/dark-mode.js" />
 
         {process.browser && (
