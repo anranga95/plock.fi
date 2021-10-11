220b989a87a65920e5792b6ada42fbe052a2f1f4
ANRanga <anranga95@gmail.com>
2021-10-11 00:33:14 -0400

    Document approach

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,12 +15,9 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="Access to the decentralised world of Celo"
         />
 
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <link rel="manifest" href="/manifest.json" />
-          <link href="/icons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
-          <link href="/icons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
-          <link rel="apple-touch-icon" href="/icons/apple-icon.png"></link>
-          <meta name="theme-color" content="#317EFB" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="/icons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
+        <link href="/icons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
 
         <script async src="/dark-mode.js" />
 
